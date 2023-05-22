7a1a3db14fb8b2943e3f4a078cdc1e7b9cd28032
Veselin <veselin.b.pavlov@gmail.com>
2023-05-22 10:20:18 +0300

    Project

--- 6. Debugging and Testing/tasks/lock.js
+++ 6. Debugging and Testing/tasks/lock.js
@@ -1,7 +1,10 @@
-task("accounts", "Prints the list of accounts",
-    async (taskArgs, hre) => {
+const { task } = require("hardhat/config");
+
+task("accounts", "Prints the list of accounts")
+    .addParam("acc", "account numbers")
+    .setAction(async (taskArgs, hre) => {
         const accounts = await hre.ethers.getSigners();
-        for (const account of accounts) {
-            console.log(account.address);
+        for (let i = 0; i < taskArgs.acc; i++) {
+            console.log(accounts[i].address);
         }
     });
