0a49bc38ed84393321531bc9d5a36174e40e8de0
RISAK\veselin <veselin.b.pavlov@gmail.com>
2023-06-07 16:23:53 +0300

    Fixing tests.

--- 12. Exam Preparation 3/test/CharityPlatformTests.js
+++ 12. Exam Preparation 3/test/CharityPlatformTests.js
@@ -7,35 +7,32 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("CharityPlatform", function () {
-  // let charityPlatformAsUser, charityPlatform, owner, user;
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  let owner, user, platform, unlockTime, goal;
 
-  // this.beforeAll(async function () {
-  //   [owner, user] = await ethers.getSigners();
-  //   const { charityPlatform } = await loadFixture(deploy);
-  //   charityPlatformAsUser = charityPlatform.connect(user);
-  // });
+  this.beforeAll(async function () {
+    [owner, user] = await ethers.getSigners();
+    const { charityPlatform } = await loadFixture(deploy);    
+    platform = charityPlatform;
+
+    const charityPlatformAsOwner = charityPlatform.connect(owner);
+    goal = ethers.utils.parseEther("2");
+    unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    await charityPlatformAsOwner.createCampaign("Test Campaign", "Test Description", goal, unlockTime);
+  });
 
   async function deploy() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-
-    const [owner, user] = await ethers.getSigners();
     const CharityPlatformFactory = await ethers.getContractFactory("CharityPlatform");
     const charityPlatform = await CharityPlatformFactory.deploy();
 
-    const charityPlatformAsOwner = charityPlatform.connect(owner);
-    const goal = ethers.utils.parseEther("1");
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-    await charityPlatformAsOwner.createCampaign("Test Campaign", "Test Description", goal, unlockTime);
-
-    return { charityPlatform, charityPlatformAsOwner, owner, user, unlockTime };
+    return { charityPlatform };
   }
 
   describe("Donation", function () {
     describe("Validations", function () {
       it("Should revert when donation is 0", async function () {
-        const { charityPlatform, user } = await loadFixture(deploy);
-        const charityPlatformAsUser = charityPlatform.connect(user);
-        const campaignId = 0;
+        const charityPlatformAsUser = platform.connect(user);
+        const campaignId = 1;
         const donation = ethers.utils.parseEther("0");
         charityPlatformAsUser.donate(campaignId, { value: donation });
         await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
@@ -43,86 +40,77 @@ describe("CharityPlatform", function () {
       });
 
       it("Should revert when capaign already ended", async function () {
-        const { charityPlatform, user, unlockTime } = await loadFixture(deploy);
-        const charityPlatformAsUser = charityPlatform.connect(user);
-        const campaignId = 0;
+        const charityPlatformAsUser = platform.connect(user);
+        const campaignId = 1;
         const donation = ethers.utils.parseEther("1");
-        await time.increase(unlockTime);
+        await time.increaseTo(unlockTime);
         charityPlatformAsUser.donate(campaignId, { value: donation });
         await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
           .to.be.revertedWith("Campaign has ended");
       });
 
       it("Should revert when capaign already reached goal", async function () {
-        const { charityPlatform, user, unlockTime } = await loadFixture(deploy);
-        await time.increase(unlockTime - 1);
-        const charityPlatformAsUser = charityPlatform.connect(user);
-        const campaignId = 0;
-        const donation = ethers.utils.parseEther("1");
+        const charityPlatformAsUser = platform.connect(user);
+        const campaignId = 1;
+        const donation = ethers.utils.parseEther("2");
         charityPlatformAsUser.donate(campaignId, { value: donation });
         await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-          .to.be.revertedWith("Campaign has reached goal");
+          .to.be.revertedWith("Campaign has reached its goal");
       });
     });
 
-    // describe("Transaction", function () {
-    //   it("Successfull donation", async function () {
-    //     const { charityPlatform, user } = await loadFixture(deploy);
-    //     const charityPlatformAsUser = charityPlatform.connect(user);
-    //     const campaignId = 0;
-    //     const donation = ethers.utils.parseEther("1");       
-    //     await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-    //       .to.emit(charityPlatformAsUser, "DonationMade")
-    //       .withArgs(campaignId, donation);
-    //   });
-    // });    
+    describe("Transaction", function () {
+      it("Successfull donation", async function () {
+        const charityPlatformAsUser = platform.connect(user);
+        const campaignId = 1;
+        const donation = ethers.utils.parseEther("1");       
+        await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
+          .to.emit(charityPlatformAsUser, "DonationMade")
+          .withArgs(campaignId, donation);
+      });
+    });    
   })
 
-  describe("FundsRelease", function () {
-    // describe("Validations", function () {
-    //   it("Should revert when campaign is not ended", async function () {
-    //     const { charityPlatform, owner } = await loadFixture(deploy);
-    //     const charityPlatformAsOwner = charityPlatform.connect(owner);
-    //     const campaignId = 0;
-    //     await time.increase(1686801559);
-    //     await expect(charityPlatformAsOwner.collectFunds(campaignId))
-    //       .to.be.revertedWith("Campaign has not reached its goal or ended yet");
-    //   });
+  // describe("FundsRelease", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert when campaign is not reach the goal", async function () {
+  //       const charityPlatformAsOwner = platform.connect(owner);
+  //       const campaignId = 1;
+  //       await expect(charityPlatformAsOwner.collectFunds(campaignId))
+  //         .to.be.revertedWith("Campaign has not reached its goal or ended yet");
+  //     });
 
-    //   it("Should revert when campaign is not ended", async function () {
-    //     const { charityPlatform, user } = await loadFixture(deploy);
-    //     const charityPlatformAsUser = charityPlatform.connect(user);
-    //     const campaignId = 0;
-    //     const donation = ethers.utils.parseEther("1");
-    //     charityPlatformAsUser.donate(campaignId, { value: donation });
-    //     await time.increase(1686801559);
-    //     await expect(charityPlatformAsOwner.collectFunds(campaignId))
-    //       .to.be.revertedWith("Campaign has not reached its goal or ended yet");
-    //   });
+  //     it("Should revert when campaign is not ended", async function () {
+  //       const charityPlatformAsUser = platform.connect(user);
+  //       const campaignId = 1;
+  //       const donation = ethers.utils.parseEther("1");
+  //       charityPlatformAsUser.donate(campaignId, { value: donation });
+  //       const charityPlatformAsOwner = platform.connect(owner);
+  //       await time.increase(2006801559);
+  //       await expect(charityPlatformAsOwner.collectFunds(campaignId))
+  //         .to.be.revertedWith("Campaign has not reached its goal or ended yet");
+  //     });
 
-    //   it("Should revert user try to release funds", async function () {
-    //     const { charityPlatform, user } = await loadFixture(deploy);
-    //     const charityPlatformAsUser = charityPlatform.connect(user);
-    //     const campaignId = 0;
-    //     await expect(charityPlatformAsUser.collectFunds(campaignId))
-    //       .to.be.revertedWith("Only the creator can release funds");
-    //   });
-    // });
+  //     it("Should revert user try to release funds", async function () {
+  //       const charityPlatformAsUser = platform.connect(user);
+  //       const campaignId = 1;
+  //       await expect(charityPlatformAsUser.collectFunds(campaignId))
+  //         .to.be.revertedWith("Only the creator can release funds");
+  //     });
+  //   });
 
-    // describe("Transaction", function () {
-    //   it("Should release funds", async function () {
-    //     const { charityPlatform, owner, user } = await loadFixture(deploy);
+  //   describe("Transaction", function () {
+  //     // it("Should release funds", async function () {
+  //     //   const charityPlatformAsUser = platform.connect(user);
+  //     //   const campaignId = 1;
+  //     //   const donation = ethers.utils.parseEther("1");
+  //     //   charityPlatformAsUser.donate(campaignId, { value: donation });
 
-    //     const charityPlatformAsUser = charityPlatform.connect(user);
-    //     const campaignId = 0;
-    //     const donation = ethers.utils.parseEther("1");
-    //     charityPlatformAsUser.donate(campaignId, { value: donation });
-
-    //     const charityPlatformAsOwner = charityPlatform.connect(owner);
-    //     await expect(charityPlatformAsOwner.collectFunds(campaignId))
-    //       .to.emit(charityPlatformAsOwner, "FundsReleased")
-    //       .withArgs(campaignId, donation);
-    //   });
-    // });
-  });
+  //     //   const charityPlatformAsOwner = platform.connect(owner);
+  //     //   await expect(charityPlatformAsOwner.collectFunds(campaignId))
+  //     //     .to.emit(charityPlatformAsOwner, "FundsReleased")
+  //     //     .withArgs(campaignId, donation);
+  //     // });
+  //   });
+  // });
 });
