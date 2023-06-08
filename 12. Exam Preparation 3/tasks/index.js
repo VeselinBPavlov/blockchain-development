f226c04f6664cbf7f7dd4a72815473f9d59a83a7
RISAK\veselin <veselin.b.pavlov@gmail.com>
2023-06-08 10:19:01 +0300

    Tests fixed.

--- 12. Exam Preparation 3/tasks/index.js
+++ 12. Exam Preparation 3/tasks/index.js
@@ -14,21 +14,23 @@ task("deploy", "Deploys a contract")
     });
 
 task("interaction", "Interaction for charity creation functionality")
+    .addParam("address", "Charity contract address")
     .addParam("name", "Charity name")
     .addParam("description", "Charity description")
     .addParam("goal", "Charity goal")
     .addParam("endtime", "Charity end time")
     .setAction(async (taskArgs, hre) => {
-        const [deployer] = await hre.ethers.getSigners();
-        const CharityFactory = await hre.ethers.getContractFactory("CharityPlatform", deployer);
-
-        const charity = await CharityFactory.attach(taskArgs.name, taskArgs.description, taskArgs.goal, taskArgs.endDate);
-        const tx = await charity.createCampaign(taskArgs.name, taskArgs.description, taskArgs.goal, taskArgs.endDate);
-        const receipt = await tx.wait();
-        if (receipt.status === 0) {
-            throw Error(`Transaction failed: ${tx.hash}`);
-        }
-
-        console.log(`Campaign created with name ${taskArgs.name} and description ${taskArgs.description}`);
-        // console.log(taskArgs.address);
+      const [deployer] = await hre.ethers.getSigners();
+      const CharityFactory = await hre.ethers.getContractFactory("CharityPlatform");
+  
+      const charity = await CharityFactory.attach(taskArgs.contractaddress);
+      const tx = await charity.createCampaign(
+        taskArgs.name,
+        taskArgs.description,
+        taskArgs.goal,
+        taskArgs.endtime
+      );
+      await tx.wait();
+  
+      console.log(`Campaign created with name ${taskArgs.name} and description ${taskArgs.description}`);
     });
--- 12. Exam Preparation 3/test/CharityPlatformTests.js
+++ 12. Exam Preparation 3/test/CharityPlatformTests.js
@@ -7,110 +7,103 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("CharityPlatform", function () {
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  let owner, user, platform, unlockTime, goal;
+  let owner, user, platform;
+  let campaignId;
 
-  this.beforeAll(async function () {
+  before(async function () {
     [owner, user] = await ethers.getSigners();
-    const { charityPlatform } = await loadFixture(deploy);    
-    platform = charityPlatform;
 
-    const charityPlatformAsOwner = charityPlatform.connect(owner);
-    goal = ethers.utils.parseEther("2");
-    unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-    await charityPlatformAsOwner.createCampaign("Test Campaign", "Test Description", goal, unlockTime);
+    // Deploy the CharityPlatform contract
+    const CharityPlatform = await ethers.getContractFactory("CharityPlatform");
+    platform = await CharityPlatform.deploy();
+    await platform.deployed();
+
+    // Create a campaign
+    await platform.createCampaign("Test Campaign", "Test Description", ethers.utils.parseEther("2"), Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30); // End date set to 30 days in the future
+    campaignId = 1; // Assuming the campaign ID is 1 (based on your previous test code)
   });
 
-  async function deploy() {
-    const CharityPlatformFactory = await ethers.getContractFactory("CharityPlatform");
-    const charityPlatform = await CharityPlatformFactory.deploy();
+  describe("donate", function () {
+    it("Should allow a user to donate to a campaign", async function () {
+      const donationAmount = ethers.utils.parseEther("1");
 
-    return { charityPlatform };
-  }
+      // User donates to the campaign
+      await platform.connect(user).donate(campaignId, { value: donationAmount });
 
-  describe("Donation", function () {
-    describe("Validations", function () {
-      it("Should revert when donation is 0", async function () {
-        const charityPlatformAsUser = platform.connect(user);
-        const campaignId = 1;
-        const donation = ethers.utils.parseEther("0");
-        charityPlatformAsUser.donate(campaignId, { value: donation });
-        await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-          .to.be.revertedWith("Donation amount must be greater than 0");
-      });
-
-      it("Should revert when capaign already ended", async function () {
-        const charityPlatformAsUser = platform.connect(user);
-        const campaignId = 1;
-        const donation = ethers.utils.parseEther("1");
-        await time.increaseTo(unlockTime);
-        charityPlatformAsUser.donate(campaignId, { value: donation });
-        await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-          .to.be.revertedWith("Campaign has ended");
-      });
-
-      it("Should revert when capaign already reached goal", async function () {
-        const charityPlatformAsUser = platform.connect(user);
-        const campaignId = 1;
-        const donation = ethers.utils.parseEther("2");
-        charityPlatformAsUser.donate(campaignId, { value: donation });
-        await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-          .to.be.revertedWith("Campaign has reached its goal");
-      });
+      // Check that the donation was successful
+      await expect(platform.connect(user).donate(campaignId, { value: donationAmount }))
+          .to.emit(platform, "DonationMade")
+          .withArgs(campaignId, donationAmount);
     });
 
-    describe("Transaction", function () {
-      it("Successfull donation", async function () {
-        const charityPlatformAsUser = platform.connect(user);
-        const campaignId = 1;
-        const donation = ethers.utils.parseEther("1");       
-        await expect(charityPlatformAsUser.donate(campaignId, { value: donation }))
-          .to.emit(charityPlatformAsUser, "DonationMade")
-          .withArgs(campaignId, donation);
-      });
-    });    
-  })
+    it("Should revert when the donation amount is 0", async function () {
+      const donationAmount = ethers.utils.parseEther("0");
 
-  // describe("FundsRelease", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert when campaign is not reach the goal", async function () {
-  //       const charityPlatformAsOwner = platform.connect(owner);
-  //       const campaignId = 1;
-  //       await expect(charityPlatformAsOwner.collectFunds(campaignId))
-  //         .to.be.revertedWith("Campaign has not reached its goal or ended yet");
-  //     });
+      // Attempt to donate 0 amount
+      await expect(platform.connect(user).donate(campaignId, { value: donationAmount })).to.be.revertedWith("Donation amount must be greater than 0");
+    });
 
-  //     it("Should revert when campaign is not ended", async function () {
-  //       const charityPlatformAsUser = platform.connect(user);
-  //       const campaignId = 1;
-  //       const donation = ethers.utils.parseEther("1");
-  //       charityPlatformAsUser.donate(campaignId, { value: donation });
-  //       const charityPlatformAsOwner = platform.connect(owner);
-  //       await time.increase(2006801559);
-  //       await expect(charityPlatformAsOwner.collectFunds(campaignId))
-  //         .to.be.revertedWith("Campaign has not reached its goal or ended yet");
-  //     });
+    it("Should revert when the campaign has ended", async function () {
+      const donationAmount = ethers.utils.parseEther("1");
 
-  //     it("Should revert user try to release funds", async function () {
-  //       const charityPlatformAsUser = platform.connect(user);
-  //       const campaignId = 1;
-  //       await expect(charityPlatformAsUser.collectFunds(campaignId))
-  //         .to.be.revertedWith("Only the creator can release funds");
-  //     });
-  //   });
+      // Increase time to make the campaign end
+      await ethers.provider.send("evm_increaseTime", [60 * 60 * 24 * 31]); // Increase time by 31 days
 
-  //   describe("Transaction", function () {
-  //     // it("Should release funds", async function () {
-  //     //   const charityPlatformAsUser = platform.connect(user);
-  //     //   const campaignId = 1;
-  //     //   const donation = ethers.utils.parseEther("1");
-  //     //   charityPlatformAsUser.donate(campaignId, { value: donation });
+      // Attempt to donate to an ended campaign
+      await expect(platform.connect(user).donate(campaignId, { value: donationAmount })).to.be.revertedWith("Campaign has ended");
+    });
 
-  //     //   const charityPlatformAsOwner = platform.connect(owner);
-  //     //   await expect(charityPlatformAsOwner.collectFunds(campaignId))
-  //     //     .to.emit(charityPlatformAsOwner, "FundsReleased")
-  //     //     .withArgs(campaignId, donation);
-  //     // });
-  //   });
-  // });
+    it("Should revert when the campaign has reached its goal", async function () {
+      // Create a new campaign
+      const endDate = 1719718614; // Set the end date to a future timestamp
+      await platform.createCampaign("New Campaign", "New Description", ethers.utils.parseEther("5"), endDate);
+    
+      const newCampaignId = 2; // Assuming the new campaign ID is 2
+      const donationAmount = ethers.utils.parseEther("5"); // Adjust the donation amount as desired
+    
+      // Donate to the new campaign to reach the goal
+      await platform.connect(user).donate(newCampaignId, { value: donationAmount });
+    
+      // Attempt to donate to the campaign that has reached its goal
+      await expect(platform.connect(user).donate(newCampaignId, { value: donationAmount })).to.be.revertedWith("Campaign has reached its goal");
+    });
+  });  
+
+  describe("collectFunds", function () {
+    it("Should allow the owner to collect funds from a campaign", async function () {
+      // Increase time to make the campaign end
+      await ethers.provider.send("evm_increaseTime", [60 * 60 * 24 * 31]); // Increase time by 31 days
+
+      // Collect funds from the campaign
+      await platform.connect(owner).collectFunds(campaignId);
+
+      // Check that the funds were collected
+      await expect(platform.connect(owner).collectFunds(campaignId))
+          .to.emit(platform, "FundsReleased");
+    });
+
+    it("Should revert when the campaign has not ended", async function () {
+      // Attempt to collect funds from a campaign that has not ended
+      const endDate = 1719718614; // Set the end date to a future timestamp
+      await platform.createCampaign("New Campaign", "New Description", ethers.utils.parseEther("5"), endDate);
+    
+      const newCampaignId = 3; // Assuming the new campaign ID is 3
+
+      await expect(platform.connect(owner).collectFunds(newCampaignId)).to.be.revertedWith("Campaign has not reached its goal or ended yet");
+    });
+
+    it("Should revert when the campaign has not reached its goal", async function () {
+      // Create a new campaign
+      const endDate = 1719718614; // Set the end date to a future timestamp
+      await platform.createCampaign("New Campaign", "New Description", ethers.utils.parseEther("5"), endDate);
+    
+      const newCampaignId = 4; // Assuming the new campaign ID is 4
+    
+      // Increase time to make the campaign end
+      await ethers.provider.send("evm_increaseTime", [60 * 60 * 24 * 31]); // Increase time by 31 days
+    
+      // Attempt to collect funds from the new campaign that has not reached its goal
+      await expect(platform.connect(owner).collectFunds(newCampaignId)).to.be.revertedWith("Campaign has not reached its goal or ended yet");
+    });
+  });
 });
